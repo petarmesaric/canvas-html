116fa5a4a821c867c5d44bf1bcac2a0b751ea46e
Nikola Milenkovic <nikola.milenkovic@blacksheep.co.rs>
2015-12-15 15:36:18 +0100

    scrolling on menu item click & new nav WIP

--- assets/js/libs/some.jquery.plugin2.js
+++ assets/js/libs/some.jquery.plugin2.js
@@ -1,1 +1,20 @@
-console.log('some jquery plugin 2');
+$(document).ready(function(){
+
+	//jQuery for page scrolling feature - requires jQuery Easing plugin
+	$(function() {
+	    $('a.page-scroll').on('click', function(event) {
+	        var $anchor = $(this);
+	        $('html, body').stop().animate({
+	            scrollTop: $($anchor.attr('href')).offset().top,
+	            // stop: $anchor.css('border', 'none')
+	        }, 1500, 'easeOutSine');
+	        event.preventDefault();
+	    });
+	});
+
+	// Closes the Responsive Menu on Menu Item Click
+	$('.navbar-collapse ul li a').click(function() {
+	    $('.navbar-toggle:visible').click();
+	});
+
+});
